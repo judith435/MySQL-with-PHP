bac108f6828657b652a10e28defad820cf70ef14
judith435 <judithguttmann@gmail.com>
2017-07-16 21:57:58 +0300

    final

--- electronics.php
+++ electronics.php
@@ -1,6 +1,7 @@
 <?php
 interface IElectronicPart {
     public function getSpecs();
+    public function insert();
 }
 
 class ElectronicPart {
@@ -27,6 +28,39 @@ class Screen extends ElectronicPart implements IElectronicPart {
             ', model: '. $this->model . 
             ', Price: ' . $this->price;
     }
+
+    public function insert() {
+
+        $host = '127.0.0.1';
+        $db   = 'electronics_l40';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+        $stmt = $pdo->prepare("insert into l40_screens (price,
+                                                        model,
+                                                        size,
+                                                        manufacturer)
+                              values  (:price,
+                                       :model,
+                                       :size,
+                                       :manufacturer)");
+
+        $stmt ->execute(array(  "price" => $this -> price, 
+                                "model" => $this -> model, 
+                                "size" => $this -> size, 
+                                "manufacturer" => $this -> manufacturer, 
+            ));
+    }
 }
 
 class Mouse extends ElectronicPart implements IElectronicPart{
@@ -45,12 +79,47 @@ class Mouse extends ElectronicPart implements IElectronicPart{
             ', model: '. $this->model . 
             ', Price: ' . $this->price;
     }
+
+        public function insert() {
+
+        $host = '127.0.0.1';
+        $db   = 'electronics_l40';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+        $stmt = $pdo->prepare("insert into l40_mouses (manufacturer,
+                                                       price,
+                                                       model,
+                                                       is_wired)
+                              values  (:manufacturer,
+                                       :price,
+                                       :model,
+                                       :is_wired)");
+
+        $stmt ->execute(array(  "manufacturer" => $this -> manufacturer, 
+                                "price" => $this -> price, 
+                                "model" => $this -> model, 
+                                "is_wired" => $this -> isWired, 
+            ));
+    }
+
+
 }
 
 class Keyboard extends ElectronicPart implements IElectronicPart{
     private $isWired;
 
-    public function __construct($manufacturer, $price, $serial, $isWired) {
+    public function __construct($manufacturer, $price, $model, $isWired) {
         $this->manufacturer = $manufacturer;
         $this->price = $price;
         $this->model = $model;
@@ -63,6 +132,40 @@ class Keyboard extends ElectronicPart implements IElectronicPart{
             ', model: '. $this->model . 
             ', Price: ' . $this->price;
     }
+
+    public function insert() {
+
+        $host = '127.0.0.1';
+        $db   = 'electronics_l40';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+        $stmt = $pdo->prepare("insert into l40_keyboards (manufacturer,
+                                                       price,
+                                                       model,
+                                                       isWired)
+                              values  (:manufacturer,
+                                       :price,
+                                       :model,
+                                       :isWired)");
+
+        $stmt ->execute(array(  "manufacturer" => $this -> manufacturer, 
+                                "price" => $this -> price, 
+                                "model" => $this -> model, 
+                                "isWired" => $this -> isWired, 
+            ));
+    }
+
 }
 
 class Computer extends ElectronicPart implements IElectronicPart{
@@ -90,6 +193,49 @@ class Computer extends ElectronicPart implements IElectronicPart{
             ', model: '. $this->model . 
             ', Price: ' . $this->price;
     }
+
+        public function insert() {
+
+        $host = '127.0.0.1';
+        $db   = 'electronics_l40';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+
+
+        $stmt = $pdo->prepare("insert into l40_computers (manufacturer,
+                                                       price,
+                                                       model,
+                                                       motherboard,
+                                                       hard_drive,
+                                                       ram,
+                                                       graphic_card)
+                              values  (:manufacturer,
+                                       :price,
+                                       :model,
+                                       :motherboard,
+                                       :hard_drive,
+                                       :ram,
+                                       :graphic_card)");
+
+        $stmt ->execute(array(  "manufacturer" => $this -> manufacturer, 
+                                "price" => $this -> price, 
+                                "model" => $this -> model, 
+                                "motherboard" => $this -> motherboard, 
+                                "hard_drive" => $this -> hardDrive,
+                                "ram" => $this -> ram,
+                                "graphic_card" => $this -> graphicCard,
+            ));
+    }
+
 }
 
 class purchase {
